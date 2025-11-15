721d64d94b142c64fc4672420f95c1b3c640ba57
Gabriel Ortega <gabriel.faia@etec.sp.gov.br>
2025-11-15 00:34:41 -0300

    Delete exercicio4 directory

--- exercicio4/script.js
+++
@@ -1,9 +0,0 @@
-function senhaForte(s) {
-    return s.length > 8 && s !== "12345678";
-}
-
-function verificar() {
-    let s = document.getElementById("senha").value;
-    document.getElementById("resultado").innerText =
-        senhaForte(s) ? "Senha Forte" : "Senha Fraca";
-}
